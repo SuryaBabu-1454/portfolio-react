b61e844d2fe5246f5e9c1c4c88a8d03c550a9a4a
SuryaBabu-1454 <suryababu1454@gmail.com>
2024-11-26 14:24:48 +0530

    Resume Updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,24 +1,39 @@
-import React, { useRef } from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile_img.jpg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-
+import React, { useRef } from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.jpg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
-
-
-  
   return (
-    <div id='home' className='hero'>
-        <img src={profile_img} alt="" />
-        <h1> <span>I'm Surya Prakash,</span> MERN Stack Developer.</h1>
-        <p>Aspiring MERN Stack developer from Chennai,India </p>
-        <div className="hero-action">
-            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
-            <div className="hero-resume"><AnchorLink className='anchor-link' offset={50} href='#resume'>My Resume</AnchorLink></div>
+    <div id="home" className="hero">
+      <img src={profile_img} alt="" />
+      <h1>
+        {" "}
+        <span>I'm Surya Prakash,</span> MERN Stack Developer.
+      </h1>
+      <p>Aspiring MERN Stack developer from Chennai,India </p>
+      <div className="hero-action">
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect With Me
+          </AnchorLink>
         </div>
+        <div className="hero-resume">
+         
+           
+            <a
+              href="/Resume.pdf" // Path to your resume file
+              target="_blank" // Open in a new tab for viewing
+              rel="noopener noreferrer"
+              download="Surya_Resume.pdf" // Optional: force download with a custom filename
+            >
+              My Resume{" "}
+            </a>
+    
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
